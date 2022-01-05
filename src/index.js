321f6dee99121857a5d5b30f2b6bd9182d1c7117
YuliyaShu <juliette7373@gmail.com>
2022-01-05 15:40:17 +0300

    docs: modife index.js

--- src/index.js
+++ src/index.js
@@ -27,35 +27,29 @@ module.exports = function toReadable (number) {
     if (String(number).length == 3 && Number(String(number).slice(1, 2)) == 0) {
         let unitsForHundred =  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
         let unitsForHundredForReturn = unitsForHundred[Number(String(number).slice(0, 1)) - 1];
-        let hundred = ['hundred', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds'];
-        let hundredForReturn = hundred[Number(String(number).slice(0, 1)) - 1];
         let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
         let unitsForReturn = units[Number(String(number).slice(-1))];
-        return `${unitsForHundredForReturn} ${hundredForReturn} ${unitsForReturn}`;
+        return `${unitsForHundredForReturn} hundred ${unitsForReturn}`;
     }
     else
     // For 110-119, 210-219, ..., 910-919
     if (String(number).length == 3 && Number(String(number).slice(1, 2)) == 1) {
         let unitsForHundred =  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
         let unitsForHundredForReturn = unitsForHundred[Number(String(number).slice(0, 1)) - 1];
-        let hundred = ['hundred', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds'];
-        let hundredForReturn = hundred[Number(String(number).slice(0, 1)) - 1];
         let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
         let teensForReturn = teens[Number(String(number).slice(-1))];
-        return `${unitsForHundredForReturn} ${hundredForReturn} ${teensForReturn}`;
+        return `${unitsForHundredForReturn} hundred ${teensForReturn}`;
     }
     else
     // For 120-199, 220-299, ..., 920-999
     if (String(number).length == 3) {
         let unitsForHundred =  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
         let unitsForHundredForReturn = unitsForHundred[Number(String(number).slice(0, 1)) - 1];
-        let hundred = ['hundred', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds', 'hundreds'];
-        let hundredForReturn = hundred[Number(String(number).slice(0, 1)) - 1];
         let dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
         let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
         let dozensForReturn = dozens[Number(String(number).slice(1, 2))-2];
         let unitsForReturn = units[Number(String(number).slice(-1))];
-        return `${unitsForHundredForReturn} ${hundredForReturn} ${dozensForReturn} ${unitsForReturn}`;
+        return `${unitsForHundredForReturn} hundred ${dozensForReturn} ${unitsForReturn}`;
     }
     
 
