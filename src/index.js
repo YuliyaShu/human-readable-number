94f239b6fb020c5b7ab859f04a69bb561754aa09
YuliyaShu <juliette7373@gmail.com>
2022-01-06 11:47:13 +0300

    docs: modify index.js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,31 @@
 module.exports = function toReadable (number) {
-    // For 0-9
-    if (String(number).length == 1) {
-        let units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        return units[number];
-    }
-    else
-    // Number(String(number).slice(0, 1)) - return first unit of number
-    // Number(String(number).slice(-1)) - return last unit of number
-    // For 10-19
-    if (String(number).length == 2 && Number(String(number).slice(0, 1)) == 1) {
-        let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-        let caseNumber = Number(String(number).slice(-1));
-        return teens[caseNumber];
-    }
-    else
-    // For 20-99
-    if (String(number).length == 2) {
-        let dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-        let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        let dozensForReturn = dozens[Number(String(number).slice(0, 1)) - 2];
-        let unitsForReturn = units[Number(String(number).slice(-1))];
-        return `${dozensForReturn} ${unitsForReturn}`.trim();
-    }
-    else
-    // For 100-109, 200-209, ..., 900-999
-    if (String(number).length == 3 && Number(String(number).slice(1, 2)) == 0) {
-        let unitsForHundred =  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        let unitsForHundredForReturn = unitsForHundred[Number(String(number).slice(0, 1)) - 1];
-        let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        let unitsForReturn = units[Number(String(number).slice(-1))];
-        return `${unitsForHundredForReturn} hundred ${unitsForReturn}`.trim();
-    }
-    else
-    // For 110-119, 210-219, ..., 910-919
-    if (String(number).length == 3 && Number(String(number).slice(1, 2)) == 1) {
-        let unitsForHundred =  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        let unitsForHundredForReturn = unitsForHundred[Number(String(number).slice(0, 1)) - 1];
-        let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-        let teensForReturn = teens[Number(String(number).slice(-1))];
-        return `${unitsForHundredForReturn} hundred ${teensForReturn}`;
-    }
-    else
-    // For 120-199, 220-299, ..., 920-999
-    if (String(number).length == 3) {
-        let unitsForHundred =  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        let unitsForHundredForReturn = unitsForHundred[Number(String(number).slice(0, 1)) - 1];
-        let dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-        let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        let dozensForReturn = dozens[Number(String(number).slice(1, 2))-2];
-        let unitsForReturn = units[Number(String(number).slice(-1))];
-        return `${unitsForHundredForReturn} hundred ${dozensForReturn} ${unitsForReturn}`.trim();
-    }
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let numberLength = String(number).length;
+    let firstUnitOfNumber = String(number)[0];
+    let lastUnitOfNumber = Number(String(number).slice(-1));
     
+    if (number == 0) {
+        return 'zero';
+    }
+    switch (numberLength) {
+        case 1: return units[number];
+        case 2: 
+            return (firstUnitOfNumber == 1 ? teens[lastUnitOfNumber] : `${dozens[firstUnitOfNumber - 2]} ${units[lastUnitOfNumber]}`.trim());
+        case 3: 
+            let middleUnitOfNumber = String(number)[1];
+            if (middleUnitOfNumber == 0) {
+                return `${units[firstUnitOfNumber]} hundred ${units[lastUnitOfNumber]}`.trim();
+            }
+            else if (middleUnitOfNumber == 1) {
+                return `${units[firstUnitOfNumber]} hundred ${teens[lastUnitOfNumber]}`;
+            }
+            return `${units[firstUnitOfNumber]} hundred ${dozens[middleUnitOfNumber-2]} ${units[lastUnitOfNumber]}`.trim();
+    };
 
     return 'Incorrect Data';
 };
+
+
